0211f01fc811eb9b8de2cc6dcaa38b27f987d9ab
MrNonoss <bruno.bordas@example.com>
2021-03-03 15:02:23 +0100

    Add mobile IP detection

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ A pretty simple bash script to query ipinfo.io<br>
 With this script, you can query ipinfo.io in an easy way.
 Two searches options are available:<br>
 --> A short one, just to get the country code<br>
---> A long one, that looks also for the ISP<br>
+--> A long one, that looks also for the ISP, AS number and guess if it's a mobile IP address, based on AS number<br>
 <br>
-Be sure to add your api key as a variable.
+Be sure to add your api key as a variable in line 33.
 You can get one (paid or free) from them.<br>
 <br>
+Usage: ./ipinfo_checker.sh entryfile outfile
 Thanks https://ipinfo.io fort his cool tool.
--- ipinfo_checker.sh
+++ ipinfo_checker.sh
@@ -4,17 +4,43 @@
 # To query ipinfo.io
 # Think about adding your token as a variable
 ###############################
-token=XXXX
+# Changelog
+# 2021-03-03: Add french mobile IPs Identification thanks to AS numbers 
+###############################
+# HELPER
+###############################
+Help() {
+  # Display Help
+  echo -e "\e[31mipinfo_cheker helper\e[0m."
+  echo
+  echo "Usage:"
+  echo "The synthax is pretty simple: \"ipinfo_checker.sh entryfile outfile \""
+  echo
+  echo "Please note two things:"
+  echo -e "-->\e[41mentryfile must contains only IPs\e[0m."
+  echo -e "-->You should first sort -u you entryfile to save your ipinfo API"
+  echo
+}
+while getopts ":h" option; do
+  case $option in
+  h) # display Help
+    Help
+    exit
+    ;;
+  esac
+done
+###############################
+token=
 clear
-read -p "Quel fichier voulez-vous traiter? " file
-read -p "Comment appelez-vous votre fichier de sortie: " done
+echo "Souvenez-vous que votre fichier à traiter ne doit contenir que des adresses IP"
 echo -e "\e[33m================================================\e[0m"
 lines=$(cat $file | wc -l)
 count=0
+#AS=$(AS3215 || AS15557 || AS12626 || AS12844 || AS5410 || AS51207 || AS25117 || AS201770)
 echo -e "\e[33m================================================\e[0m"
 echo "Vous avez la possibilité d'effectuer deux types de recherches:"
 echo "-> Une recherche courte, uniquement pour le Pays (c)"
-echo "-> Une recherche longue avec le FAI (l)"
+echo "-> Une recherche longue avec le FAI et une indication d'IP mobile ou non (l)"
 echo -e "\e[33m================================================\e[0m"
 echo -e "\e[1;31mRappelez vous que vous avez $lines lignes a traiter\e[0m"
 echo -e "\e[33m================================================\e[0m"
@@ -28,10 +54,10 @@ while true
      [c])
      while read ip
        do a=$(curl --silent ipinfo.io/$ip/country?token=$token)
-	   echo $ip $a >> $done
+	   echo $ip $a >> $2
 	   ((count++))
 	   echo "--> $count ligne(s) sur $lines traitée(s)"
-     done < $file
+     done < $1
      echo -e "\e[1;41mJ'ai fini!\e[0m"
   break
   ;;
@@ -40,10 +66,14 @@ while true
 ##############################################################
      [l])
      while read ip
-       do curl --silent ipinfo.io/$ip?token=$token | grep '"ip"\|"country"\|"org"' | tr -d '"' | tr -d " " | cut -d ":" -f2 | tr -s "\n" "," | awk -F "," '{print $1,$2,$3}' >> $done
+       do lQueryIP=$(curl --silent ipinfo.io/$ip?token=$token | grep '"ip"\|"country"\|"org"' | tr -s "\n" "," | awk -F '"' '{print $4","$8","$12}')
+	   if [[ "$lQueryIP" =~ .*"AS3215".* ]] || [[ "$lQueryIP" =~ .*"AS15557".* ]] || [[ "$lQueryIP" =~ .*"AS12626".* ]] || [[ "$lQueryIP" =~ .*"AS12844".* ]] || [[ "$lQueryIP" =~ .*"AS5410".* ]] || [[ "$lQueryIP" =~ .*"AS51207".* ]] || [[ "$lQueryIP" =~ .*"AS25117".* ]] || [[ "$lQueryIP" =~ .*"AS201770".* ]]
+		then echo $lQueryIP",Mobile" >> $2
+	   else echo $lQueryIP >> $2
+	   fi
 	   ((count++))
 	   echo "--> $count ligne(s) sur $lines traitée(s)"
-     done < $file
+     done < $1
      echo -e "\e[1;41mJ'ai fini!\e[0m"
   break
   ;;
